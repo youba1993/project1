1f4d22d926f77fff268a236d523e7a091277dc56
youba idjis <youba.etud@gmail.com>
2023-01-20 18:09:01 -0800

    server and routes

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 require('dotenv').config()
 const express = require('express');
+const workoutRoutes = require('./routes/workouts')
 
 //express app created
 const app = express()
 
-//routes
-app.get("/", (req,res)=>{
-    res.json({msg: 'welcome to the app'})
+//middleware
+// access the req data
+app.use(express.json())
+
+app.use((req, res, next)=>{
+    console.log(req.path, req.method)
+    next()
 })
 
-app.get("/hello", (req,res)=>{
-    res.json({msg: 'Hello world'})
-})
+//routes
+app.use('/api/workouts', workoutRoutes)
 
 //listen for requests on port 4000
 app.listen(process.env.PORT, ()=>{
