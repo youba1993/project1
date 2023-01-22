e680f97791b0a3fd98a85e7662003994536e54e4
youba idjis <youba.etud@gmail.com>
2023-01-21 22:31:43 -0800

    adding controllers

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,31 +1,20 @@
 const express = require('express');
-const Workout = require('../models/workoutModel')
+const {
+    createWorkout,
+    getWorkouts,
+    getSingle
+} = require("../controllers/workoutController")
 
 const router = express.Router();
 
 //GET all workouts
-router.get("/", (req, res) => {
-    res.json({ msg: 'GET all workouts' })
-})
+router.get("/", getWorkouts)
 
 // GET a single workouts
-router.get('/:id', (req, res) => {
-    res.json({ msg: 'GET a single workout' })
-})
+router.get('/:id', getSingle)
 
 // POST a new workout
-router.post('/', async (req, res) => {
-    const { title, load, reps } = req.body
-
-    try {
-        const workout = await Workout.create({ title, load, reps })
-        res.status(200).json(workout)
-        
-    } catch (error) {
-        res.status(400).json({error: error.message})
-        return;
-    }
-})
+router.post('/', createWorkout)
 
 //DELETE a workout
 router.delete('/:id', (req, res) => {
