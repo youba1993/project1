f8e67792c3c87df02092c210c5fefd72944fb9b4
youba idjis <youba.etud@gmail.com>
2023-01-24 21:27:23 -0800

    adding AUTH

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+
+
+// login user
+const loginUser = async (req, res)=>{
+    res.json({msg: 'user login'})
+}
+
+//signup user
+const signupUser = async (req, res)=>{
+    res.json({msg: 'user signup'})
+}
