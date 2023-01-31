074c19d5b45226e0cc331e15b531d8917ac3ac63
youba idjis <youba.etud@gmail.com>
2023-01-30 21:03:25 -0800

    user signup auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,13 @@ const loginUser = async (req, res) => {
 
 //signup user
 const signupUser = async (req, res) => {
-    res.json({ msg: 'user signup' })
+    const {email, password} = req.body
+    try {
+        const user = await User.signup(email, password)
+        res.status(200).json({email, user})
+    }catch (error){
+        res.status(400).json({error: error.message})
+    }
 }
 
 module.exports = {
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 const Schema = mongoose.Schema;
 
@@ -15,13 +16,17 @@ const userSchema = new Schema({
 })
 
 // Stastic signup method
-// userSchema.statics.signup = async (email, password) => {
-//     const exists = await this.findOne({email});
-//     if (exists) {
-//         throw Error('email already exist'){
+userSchema.statics.signup = async function (email, password) {
+    const exists = await this.findOne({email});
+    if (exists) {
+        throw Error('email already exist')
+    }
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
 
-//         }
-//     }
-// }
+    const user = await this.create({ email, password: hash})
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
