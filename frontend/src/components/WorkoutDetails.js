172d6cd512d67700f57a324ea97db7eaa5ca03f3
youba idjis <youba.etud@gmail.com>
2023-02-02 13:58:11 -0800

    making auth requests to the app

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,16 +1,25 @@
 import { useWorkoutContext } from "../hooks/useWorkoutContext";
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import { useAuthContext } from "../hooks/useAuthContext";
 
-function WorkoutDetails({workout}){
-    const {dispatch }  = useWorkoutContext()
+function WorkoutDetails({ workout }) {
+    const { dispatch } = useWorkoutContext()
+    const { user } = useAuthContext();
 
     const handleClick = async () => {
-        const response = await fetch('/api/workouts/'+ workout._id, {
-            method: 'DELETE'
+        if (!user){
+            return
+        }
+
+        const response = await fetch('/api/workouts/' + workout._id, {
+            method: 'DELETE',
+            headers: {
+                'authorization': `bearer ${user.token}`
+            }
         })
         const json = await response.json()
-        if (response.ok){
-            dispatch({type: 'DELETE_WORKOUT', payload: json})
+        if (response.ok) {
+            dispatch({ type: 'DELETE_WORKOUT', payload: json })
         }
     }
     return (
@@ -18,7 +27,7 @@ function WorkoutDetails({workout}){
             <h4>{workout.title}</h4>
             <p><strong>Load (kg): </strong>{workout.load}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
-            <p>{formatDistanceToNow(new Date(workout.createdAt), {addSuffix: true})}</p>
+            <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
             <span className="material-symbols-outlined" onClick={handleClick}>Delete</span>
         </div>
     )
--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,22 +1,30 @@
 import { useState } from "react";
 import { useWorkoutContext } from "../hooks/useWorkoutContext";
-
+import { useAuthContext } from "../hooks/useAuthContext";
 function WorkoutForm() {
     const { dispatch } = useWorkoutContext()
     const [title, setTitle] = useState('');
     const [load, setLoad] = useState('');
     const [reps, setReps] = useState('');
     const [error, setError] = useState(null);
+    const { user } = useAuthContext();
+
     // const [emptyFields, setEmptyFields] = useState([])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
+        if (!user) {
+            setError('You must be logged in')
+            return
+        }
+
         const workout = { title, load, reps };
         const response = await fetch('/api/workouts', {
             method: "POST",
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'authorization': `bearer ${user.token}`
             },
             body: JSON.stringify(workout)
         });
@@ -47,7 +55,7 @@ function WorkoutForm() {
                 type='text'
                 onChange={(e) => setTitle(e.target.value)}
                 value={title}
-                // className={!!emptyFields.includes('title') ? 'error' : ''}
+            // className={!!emptyFields.includes('title') ? 'error' : ''}
             />
 
             <label>Load (in kg):</label>
@@ -56,7 +64,7 @@ function WorkoutForm() {
                 type='number'
                 onChange={(e) => setLoad(e.target.value)}
                 value={load}
-                // className={!!emptyFields.includes('load') ? 'error' : ''}
+            // className={!!emptyFields.includes('load') ? 'error' : ''}
             />
 
             <label>Reps:</label>
@@ -65,7 +73,7 @@ function WorkoutForm() {
                 type='number'
                 onChange={(e) => setReps(e.target.value)}
                 value={reps}
-                // className={!!emptyFields.includes('reps') ? 'error' : ''}
+            // className={!!emptyFields.includes('reps') ? 'error' : ''}
             />
             <button>Add Workout</button>
             {error && <div className="error">{error}</div>}
