aa8f57a16eb86f6dbb51587f038a92634b4b963f
youba idjis <youba.etud@gmail.com>
2023-02-02 13:44:35 -0800

    adding auth to fetch all workouts

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,27 +3,36 @@ import { useEffect } from "react";
 import WorkoutDetails from "../components/WorkoutDetails"
 import WorkoutForm from "../components/WorkoutForm";
 import { useWorkoutContext } from "../hooks/useWorkoutContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 function Home() {
-    const {workouts, dispatch} = useWorkoutContext();
+    const { workouts, dispatch } = useWorkoutContext();
+    const { user } = useAuthContext()
 
-    useEffect(()=>{
+    useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch('/api/workouts');
+            const response = await fetch('/api/workouts', {
+                headers: {
+                    'authorization': `bearer ${user.token}`
+                }  
+            });
             const json = await response.json()
 
-            if (response.ok){
-                dispatch({type: 'SET_WORKOUTS', payload: json})
+            if (response.ok) {
+                dispatch({ type: 'SET_WORKOUTS', payload: json })
             }
         }
-        fetchWorkouts()
-    },[dispatch])
+        if (user) {
+            fetchWorkouts()
+        }
+
+    }, [dispatch, user])
 
     return (
         <div className="home">
             <div className="workouts">
-                {workouts && workouts.map((workout)=>(
-                    <WorkoutDetails key={workout._id} workout={workout}/>
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout} />
                 ))}
             </div>
             <WorkoutForm />
