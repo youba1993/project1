6fcf9c05e5d5462a4eb882b9497d08db54d5b7d0
youba idjis <youba.etud@gmail.com>
2023-02-02 15:03:55 -0800

    assigning workouts to users

--- backend/models/workoutModel.js
+++ backend/models/workoutModel.js
@@ -14,6 +14,10 @@ const workoutsSchema= new Schema({
     load: {
         type: Number,
         required: true
+    },
+    user_id: {
+        type: String,
+        required: true
     }
 }, { timestamps: true });
 
--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,13 +1,16 @@
-import { useAuthContext } from './useAuthContext'
+import { useAuthContext } from './useAuthContext';
+import { useWorkoutContext } from './useWorkoutContext';
+
 export const useLogout = () => {
-    const { dispatch } = useAuthContext()
+    const { dispatch } = useAuthContext();
+    const { dispatch: workoutDispatch } = useWorkoutContext();
     const logout = () => {
         // remove user from storage
         localStorage.removeItem('user');
 
         //dispatch logout action 
         dispatch({type: 'LOGOUT'})
-
+        workoutDispatch({type: 'SET_WORKOUTS', payload: null})
     }
     return {logout}
 }
